97e090f6737db849dcadc3ae859ac93b2e087444
JNic812 <john@friends.geosite.io>
2021-02-17 10:04:42 -0500

    started ClickCounter module

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 
 import './App.css';
+import ClickCounter from './ClickCounter/ClickCounter';
 
 function App() {
   return (
     <div className="App">
     <h1>Fudgestick</h1>
+    <ClickCounter />
+
+    <button>Increase Count</button>
     </div>
   );
 }
