50e46c7f0c5d0d02fab709af1c5a259ad7a862cd
JWNicholson <john.nicholson812@gmail.com>
2021-02-20 14:07:51 -0500

    initial Jotto setup

--- src/App.js
+++ src/App.js
@@ -1,47 +1,19 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
+import Jotto from './Jotto/Jotto';
+import PageTitle from './PageTitle/PageTitle';
+//import ClickCounter from './ClickCounter/ClickCounter';
 
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [showError, setShowError] = useState(false);
-
-
-  const incrementHandler = () => {
-
-    if (showError) { setShowError(false); }
-    setCount(prevCount => prevCount + 1);
-
-  }
-
-  const decrementHandler = () => {
-
-    if (count > 0) {
-      setCount(prevCount => prevCount - 1);
-    } else {
-      setShowError(true)
-    }
-
-  }
-
 
   return (
     <div data-test="component-app" className="App" >
-      <h1 data-test="counter-display">Der Zählung is&nbsp;<span data-test="count">{count}</span></h1>
+      <PageTitle />
 
-      <div data-test="error-message" className={`error ${showError ? '' : 'hidden'}`}>
-        The counter cannot go below 0!!
-      </div>
+      {/* <ClickCounter /> */}
 
-      <button
-        data-test="increment-button"
-        onClick={incrementHandler}
-      >Zuwach</button>
-
-      <button
-        data-test="decrement-button"
-        onClick={decrementHandler}
-      >Dekrement</button>
+      <Jotto />
 
     </div>
   )
--- src/ClickCounter/ClickCounter.js
+++ src/ClickCounter/ClickCounter.js
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const ClickCounter = () => {
+    const [count, setCount] = useState(0);
+    const [showError, setShowError] = useState(false);
+
+
+    const incrementHandler = () => {
+
+        if (showError) { setShowError(false); }
+        setCount(prevCount => prevCount + 1);
+
+    }
+
+    const decrementHandler = () => {
+
+        if (count > 0) {
+            setCount(prevCount => prevCount - 1);
+        } else {
+            setShowError(true)
+        }
+
+    }
     return (
         <div>
-            <h3>The Count is</h3>
+            <h1 data-test="counter-display">Der Zählung is&nbsp;<span data-test="count">{count}</span></h1>
+
+            <div data-test="error-message" className={`error ${showError ? '' : 'hidden'}`}>
+                The counter cannot go below 0!!
+</div>
+
+            <button
+                data-test="increment-button"
+                onClick={incrementHandler}
+            >Zuwach</button>
+
+            <button
+                data-test="decrement-button"
+                onClick={decrementHandler}
+            >Dekrement</button>
         </div>
     )
 }
