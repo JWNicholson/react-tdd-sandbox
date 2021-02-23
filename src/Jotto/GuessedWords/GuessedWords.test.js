793f48b4331ea3fac52e3b03aa2fd12054de1ddd
JWNicholson <john.nicholson812@gmail.com>
2021-02-23 10:15:42 -0500

    Added GuessedWords and setup for GuessedWords tests

---
+++ src/Jotto/GuessedWords/GuessedWords.test.js
@@ -0,0 +1,6 @@
+import { EnzymeAdapter } from 'enzyme';
+import React from 'react';
+import { Shallow } from 'enzyme';
+import { findTestByAttr, checkProps } from '../../tests/testUtils';
+import GuessedWords from './GuessedWords';
+
